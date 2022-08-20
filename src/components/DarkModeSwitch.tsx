d08d68d49fc7c3ea5f9dc55d05daa8a8b73f9fff
Christopher Bunn <github@thenerdery.org>
2022-08-20 09:26:21 +0900

    Remove color theme from dark mode switch.

--- src/components/DarkModeSwitch.tsx
+++ src/components/DarkModeSwitch.tsx
@@ -8,7 +8,6 @@ export const DarkModeSwitch = () => {
     <IconButton
       icon={isDark ? <SunIcon /> : <MoonIcon />}
       aria-label="Toggle Theme"
-      colorScheme="green"
       onClick={toggleColorMode}
     />
   );
