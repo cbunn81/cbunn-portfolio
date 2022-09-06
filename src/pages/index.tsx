962bb689de5c34168eb0bd5a98017d8fbdbcf50f
Christopher Bunn <github@thenerdery.org>
2022-09-07 02:24:23 +0900

    Add link to resume PDF along with the PDF file itself.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -58,8 +58,14 @@ const Index = () => (
           <Text fontSize="xl">
             My name is Chris, and I'm a software developer. I'm originally from
             Philadelphia in the US, but I'm currently living in Tokushima,
-            Japan. Please have a look at some of my recent projects, and feel
-            free to{" "}
+            Japan. Please have a look at some of my recent projects, check out{" "}
+            <Link
+              href="/christopher_bunn-resume.pdf"
+              textDecoration="underline 1px"
+            >
+              my resume
+            </Link>
+            , and feel free to{" "}
             <Link href="mailto:cbunn@cbunn.com" textDecoration="underline 1px">
               contact me
             </Link>
